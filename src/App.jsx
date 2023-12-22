5a28adfec164b6d7aab205e5a998d125d8e51726
Eyuel <eyumanfrew@gmail.com>
2023-12-22 22:44:50 +0300

    Changed hello world

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,3 @@
 export default function App() {
-  return (
-    <h1 className=" flex text-red-500">
-      Hello world!
-    </h1>
-  )
+  return <h1 className=" flex text-red-500">joel frew</h1>;
 }
